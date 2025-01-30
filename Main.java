291b7eeec1f5b6455264f24c0a78037cae33151d
Nolan McCormick <nolan.paul.mccormick@gmail.com>
2025-01-30 09:51:16 -0800

    added comments for goals to complete

--- Main.java
+++ Main.java
@@ -1,8 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+
         for (int num : arr) {
-            System.out.print(num + " ")
+            System.out.print(num + " ");
+            sum += num;
         }
+        System.out.println("\nSum: " + sum);
     }
 }
+
+/*
+ * Find the mean
+ * Find the smallest number
+ * Find the biggest number
+ * Mind the mode (most common num)
+ * 
+ * Reverse the array into a new array
+ * SHift the array over by a variable int
+ * (moving the last to the front)
+ */
